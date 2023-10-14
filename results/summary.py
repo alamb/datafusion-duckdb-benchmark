5d09a6e64d0c867441f3c48cc08f6c42094dbd62
Andrew Lamb <andrew@nerdnetworks.org>
2023-10-14 15:05:03 -0400

    Also write overall.csv (#16)
    * Also write overall.csv
    
    * fix
    
    * remove python min/max -- will use spreadsheet

--- results/summary.py
+++ results/summary.py
@@ -1,3 +1,7 @@
+# Generates summary results from the data in
+# latest/{benchmark}_{duckdb|datafusion}
+
+
 from prettytable import PrettyTable
 import os
 
@@ -21,6 +25,7 @@ def read_file(file_path):
     return data
 
 
+# remove any keys that only appear in duckdb_results
 def fix_duckdb_results(datafusion_results, duckdb_results):
     new_duckdb_results = dict()
     for k in datafusion_results.keys():
@@ -29,6 +34,16 @@ def fix_duckdb_results(datafusion_results, duckdb_results):
 
 
 if __name__ == "__main__":
+
+    # generate csv table to `latest/{overall.csv}`
+    #
+    # Example:
+    #
+    # Benchmark,Query,Datafusion,DuckDB
+    # tpch1,16.39,8.1,2.02x slower
+    csv_table = list()
+    csv_table.append("Benchmark,Query,Datafusion,DuckDB")
+
     benches = ["tpch", "h2o", "clickbench"]
     for bench in benches:
         table = list()
@@ -42,8 +57,6 @@ if __name__ == "__main__":
         datafusion_results, duckdb_results = fix_duckdb_results(datafusion_results, duckdb_results)
 
         # track overall best/worst times
-        best_times = 1
-        worst_times = 0
         print(f"{bench}: ")
         table.append(["Query", "DataFusion", "DuckDB", "Summary (Datafusion / DuckDB))"])
         for df_key, duck_key in zip(datafusion_results.keys(), duckdb_results.keys()):
@@ -52,12 +65,8 @@ if __name__ == "__main__":
 
             times = round(df_res / duck_res, 4)
             if times > 1:
-                if worst_times < times:
-                    worst_times = times
                 times = f"{round(times, 2)}x slower"
             else:
-                if best_times > times:
-                    best_times = times
                 times = f"{round(1/times, 2)}x faster"
 
             table.append([df_key, round(df_res, 2), round(duck_res, 2), times])
@@ -66,17 +75,13 @@ if __name__ == "__main__":
         tab.add_rows(table[1:])
         print(tab)
 
-        print(f"Best is {round(1/best_times, 2)}x faster")
-        print(f"Worst is {round(worst_times, 2)}x slower")
-        print("\n")
-
-        # generate latex table
+        # generate latex table to `latest/{bench.tex`
         latex_table = list()
         latex_table.append("\\begin{table}[h]")
         latex_table.append("\\centering")
         latex_table.append("\\begin{tabular}{|l|l|l|l|}")
         latex_table.append("\\hline")
-        latex_table.append("Query & DataFusion & DuckDB & Summary (Datafusion / DuckDB) \\\\")
+        latex_table.append("Query & \textbf{DataFusion} & \textbf{DuckDB} & Delta \\\\")
         latex_table.append("\\hline")
         for row in table[1:]:
             latex_table.append(" & ".join([str(x) for x in row]) + " \\\\")
@@ -90,3 +95,13 @@ if __name__ == "__main__":
         with open(output_filename, "w") as f:
             print("Writing tex based tables to {}".format(output_filename))
             f.write("\n".join(latex_table))
+
+        # update the csv table
+        for row in table[1:]:
+            csv_table.append(bench + "," + ",".join([str(x) for x in row]))
+
+
+    output_filename = f"latest/overall.csv"
+    with open(output_filename, "w") as f:
+        print("Writing overall summary to {}".format(output_filename))
+        f.write("\n".join(csv_table))
